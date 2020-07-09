cdbf1b19af655ff48719b413a9f4ee526f9caf87
Deer5000 <khidrbrinkley32@gmail.com>
2020-07-08 20:04:33 -0400

    finished

--- music/urls.py
+++ music/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
   path('', views.musician_list, name= 'musician'),
+  path("musician/<int:musician_id>/", views.album_list, name= "album_list"),
+  path("album/<int:album_id>/", views.album_details, name = "album_details")
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Musician
+from .models import Album
 
 def home(request):
     return HttpResponse("Hello, world. This is my music site!")
@@ -14,7 +15,15 @@ def musician_list(request):
     return render(request, 'musician_list.html', context)
 
 def album_list(request, musician_id):
-    musician =  Musician.objects.get(id = musician_id),
+    musician =  Musician.objects.get(id = musician_id)
     albums =  musician.album_set.all()
-    context = { 'musican': musician, 'albums': albums}
+    context = { 'musician': musician, 'albums': albums}
     return render(request, 'musician_details.html', context)
+
+
+def album_details(request, album_id):
+    album = Album.objects.get(id = album_id)
+    songs =  album.song_set.all()
+    context = { 'album': album, 'songs': songs}
+    return render(request, 'album_details.html', context)
+
